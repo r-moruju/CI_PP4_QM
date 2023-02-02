3b40b6d1ef603c077ffa8405f103137df45ffc89
R Moruju <morujurazvan@gmail.com>
2023-02-02 16:16:02 +0000

    Create 'Car' model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Car(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,
+                              related_name='cars')
+    reg_number = models.CharField(max_length=10, blank=False, null=False)
+    mot_expire_date = models.DateField(blank=False, null=False)
+    make = models.TextField(blank=False, null=False)
+    model = models.TextField(blank=False, null=False)
+    manufacturing_year = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.make} {self.model} from {self.manufacturing_year}"
