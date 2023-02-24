5be47ccbc9e3941633a372afa658b8a632fd1675
R Moruju <morujurazvan@gmail.com>
2023-02-24 12:01:32 +0000

    Change Debug to False

--- quick_mot/settings.py
+++ quick_mot/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["quick-mot.herokuapp.com", "localhost", '127.0.0.1']
 
