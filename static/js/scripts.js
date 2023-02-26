1cfcf2eb0a772f27fcbf9ec9452b9b15f42f7a52
R Moruju <morujurazvan@gmail.com>
2023-02-26 14:55:09 +0000

    Add comments to js file

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,16 +1,20 @@
+// Automatically dismiss messages as in CI Django 'code-star' project
 setTimeout(function () {
     let message = document.getElementById('msg');
-    let alert = new bootstrap.Alert(message);
-    alert.close();
+    let feedback = new bootstrap.Alert(message);
+    feedback.close();
 }, 3000);
 
 
+// Activates Bootstarp Carousel as in Bootstrap documentation
 const bookingsCarousel = document.querySelector('#bookings_carousel')
 const carousel = new bootstrap.Carousel(bookingsCarousel, {
   ride: false
 })
 
 
+// Code from https://medium.com/geekculture/django-implementing-star-rating-e1deff03bb1c
+// Use user rating and update average by calling 'rate' view
 const rate = (rating, site_id) => {
   fetch(`/rate/${site_id}/${rating}/`, {
       method: 'GET',
